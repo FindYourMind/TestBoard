6fd2a589180e61564a8d264c46453b47e9f07c1b
Fedor <f.zavyalov@mail.ru>
2023-10-17 00:11:34 -0700

    Тестовый проект. Отработка базовых элементов Storyboard. Изменение изображения Луна - Солнце.

--- TestBoard/ViewController.swift
+++ TestBoard/ViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var changeButton: UIButton!
+    
+    @IBOutlet weak var sunImageView: UIImageView!
+    
+    private var isSun: Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        skyUpdate()
+        
+        
     }
 
+    fileprivate func skyUpdate() {
+        if isSun {
+            sunImageView.image = UIImage(systemName: "sun.max")
+            sunImageView.tintColor = .orange
+            view.backgroundColor = .white
+            changeButton.backgroundColor = UIColor.black
+            changeButton.tintColor = UIColor.white
+        } else {
+            sunImageView.image = UIImage(systemName: "moon")
+            sunImageView.tintColor = .yellow
+            view.backgroundColor = .black
+            changeButton.backgroundColor = UIColor.white
+            changeButton.tintColor = UIColor.black
+        }
+    }
+    
+    @IBAction func buttonDidTap(_ sender: Any) {
+        print("Выотпустили кнопку.")
+        isSun.toggle()
+        skyUpdate()
 
+    }
+    
+    @IBAction func buttonTap(_ sender: Any) {
+        print("Вы нажали кнопку, но ещё не отпустили.")
+    }
 }
 
